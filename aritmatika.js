a5caad5cc1bb672beeef3e7f2c4d464d4e8687f2
Y0RD4NN <140366798+Y0RD4NN@users.noreply.github.com>
2023-07-25 14:55:44 +0700

    Create aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,14 @@
+ let bil1=8;
+ let bil2=7;
+//Operasi Aritmatika 
+ let pertambahan=bil1+bil2;
+ let pengurangan=bil1-bil2;
+ let perkalian=bil1*bil2;
+ let pembagian=bil1/bil2;
+ let modulus=bil1%bil2;
+ document.write ("Hasil Pertambahan " +pertambahan + "<br>");
+ document.write ("Hasil Pengurangan " +pengurangan + "<br>");
+ document.write ("Hasil perkalian " +perkalian + "<br>");
+ document.write ("Hasil Pembagian " +pembagian + "<br>");
+ document.write ("Hasil modulus " +modulus + "<br>");
+   
